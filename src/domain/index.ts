ca657fcf4aa7eb63819dbf0abc24f7bf50b59e90
Johannes Kettmann <johannes.kettmann@dkb.de>
2024-04-05 08:55:46 +0200

    Add domain types

---
+++ src/domain/index.ts
@@ -0,0 +1,27 @@
+export interface User {
+  id: string;
+  handle: string;
+  avatar: string;
+  info?: string;
+}
+
+export interface Me extends User {
+  followerIds: string[];
+}
+
+export interface Shout {
+  id: string;
+  createdAt: number;
+  authorId: string;
+  text: string;
+  likes: number;
+  reshouts: number;
+  image?: Image;
+  replies: string[];
+  replyTo?: string;
+}
+
+export interface Image {
+  id: string;
+  url: string;
+}
